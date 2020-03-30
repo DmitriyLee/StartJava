fdeac63ae5544c7db74a3096048f8dc8391c0828
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-03-30 22:20:42 +0300

    Fixed bugs in the file - Variable.java

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -6,7 +6,7 @@ public class Variable {
 		long tdp = 53;
 		float cpu = 3.10f;
 		double ram = 8.00d;
-		boolean theData = true;
+		boolean isWindowsOs = true;
 		char name = 'C';
 
 		System.out.println(core + "-Core");
@@ -15,7 +15,7 @@ public class Variable {
 		System.out.println(tdp + "-TDP");
 		System.out.println(cpu + "-CPU");
 		System.out.println(ram + "-RAM");
-		System.out.println(theData + "-The data is correct or not");
+		System.out.println(isWindowsOs + "-The data is correct or not");
 		System.out.println(name + "omputer properties");
 	}
 }
