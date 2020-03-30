83de0232c39a6bca47e11faee35e82ff39ddb46d
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-03-30 16:22:28 +0300

    Все файлы добавил в папку Lesson1

---
+++ Lesson1/Biti.java
@@ -0,0 +1,9 @@
+public class Biti {
+	public static void main(String[] args) {
+		//byte c, a, t;//объявили сразу три переменные
+		byte number = 25;
+		byte number2 =number + 10;
+		byte number3 = number2 + 20;
+		System.out.println(number3);
+	}
+}
---
+++ Lesson1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte core = 2;
+		short typeOfSystem = 64;
+		int systemBusFreqvency = 5;
+		long TDP = 53;
+		float CPU = 3.10f;
+		double RAM = 8.00d;
+		boolean isTrue = true;
+		char name = 'C';
+		String str = "omputer properties";
+	System.out.println(core + "-Core");
+	System.out.println(typeOfSystem + "-Type Of System");
+	System.out.println(systemBusFreqvency + "-System Bus Freqvency");
+	System.out.println(TDP + "-TDP");
+	System.out.println(CPU + "-CPU");
+	System.out.println(RAM + "-RAM");
+	System.out.println(isTrue + "-True or false");
+	System.out.println(name + str);
+	}
+}
