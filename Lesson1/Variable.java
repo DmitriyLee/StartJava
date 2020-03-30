60a7f927ee16a97ebb76e773c9480239d33a79f0
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-03-30 22:04:12 +0300

    File deleted - BreakFast.java, Fixed bugs in the file - Variable.java

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -2,20 +2,20 @@ public class Variable {
 	public static void main(String[] args) {
 		byte core = 2;
 		short typeOfSystem = 64;
-		int systemBusFreqvency = 5;
-		long TDP = 53;
-		float CPU = 3.10f;
-		double RAM = 8.00d;
-		boolean isTrue = true;
+		int systemBusFrequency = 5;
+		long tdp = 53;
+		float cpu = 3.10f;
+		double ram = 8.00d;
+		boolean theData = true;
 		char name = 'C';
-		String str = "omputer properties";
-	System.out.println(core + "-Core");
-	System.out.println(typeOfSystem + "-Type Of System");
-	System.out.println(systemBusFreqvency + "-System Bus Freqvency");
-	System.out.println(TDP + "-TDP");
-	System.out.println(CPU + "-CPU");
-	System.out.println(RAM + "-RAM");
-	System.out.println(isTrue + "-True or false");
-	System.out.println(name + str);
+
+		System.out.println(core + "-Core");
+		System.out.println(typeOfSystem + "-Type Of System");
+		System.out.println(systemBusFrequency + "-System Bus Freqvency");
+		System.out.println(tdp + "-TDP");
+		System.out.println(cpu + "-CPU");
+		System.out.println(ram + "-RAM");
+		System.out.println(theData + "-The data is correct or not");
+		System.out.println(name + "omputer properties");
 	}
 }
