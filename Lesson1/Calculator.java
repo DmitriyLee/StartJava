67861cbe1b5a182d5e9b5ee53c6e5971eaa38a97
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-04-18 20:16:59 +0300

    Исправленны неточности в коде Calculator

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -7,26 +7,21 @@ public class Calculator {
 		if (sign == '+') {// сложение
 			int sum = a + b;
 			System.out.println(sum);
-		}
-
-		else if (sign == '-') {//вычитание
+		} else if (sign == '-') {//вычитание
 			int minus = a - b;
 			System.out.println(minus);
-		}
-
-		else if (sign == '*') {//умножение
+		} else if (sign == '*') {//умножение
 			int multiply = a * b;
 			System.out.println(multiply);
-		}
-
-		else if (sign == '%') {//деление по модулю
+		} else if (sign == '%') {//деление по модулю
 			int moduldivision = a % b;
 			System.out.println(moduldivision);
-		}
-
-		else if (sign == '^') {//возведение в степень
+		} else if (sign == '^') {//побитовое исключающeе OR (или)
 			int exponent = a ^ b;
-			System.out.println(exponent);
+			System.out.println(b);
+			System.out.println("Number: " + a + " in binary notation: " + Integer.toBinaryString(a));
+			System.out.println("Number: " + b + " in binary notation: " + Integer.toBinaryString(b));
+			System.out.println("Number: " + exponent + " in binary notation: " + Integer.toBinaryString(exponent));
 		}
 	}
 }
