0974d0bd4d51af12e86f4c78d5e1bbe96650a9d2
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-04-18 18:56:22 +0300

    Добавил программу Калькулятор

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int a = 8;
+		int b = 5;
+		char sign = '^';
+
+		if (sign == '+') {// сложение
+			int sum = a + b;
+			System.out.println(sum);
+		}
+
+		else if (sign == '-') {//вычитание
+			int minus = a - b;
+			System.out.println(minus);
+		}
+
+		else if (sign == '*') {//умножение
+			int multiply = a * b;
+			System.out.println(multiply);
+		}
+
+		else if (sign == '%') {//деление по модулю
+			int moduldivision = a % b;
+			System.out.println(moduldivision);
+		}
+
+		else if (sign == '^') {//возведение в степень
+			int exponent = 3;
+				exponent = exponent * a;
+			System.out.println(exponent);
+		}
+	}
+}
