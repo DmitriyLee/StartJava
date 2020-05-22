fc7324ba200ff0efa946037f6758206d3089ad85
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-05-22 23:38:37 +0300

    Исправлены неточности в коде

--- Lesson1/GuessGame.java
+++ Lesson1/GuessGame.java
@@ -1,16 +1,16 @@
-public class GuesGame {
-	public static int playerNumber = 47;
+public class GuessGame {
+	public static int playerNumber = 23;
 	public static int a = 53;
 	public static int c = 99;
 	public static int m = 83;
 	public static int b = 2;
-	public static int receivedRandomNumber() {
+	private static int receivedRandomNumber() {
 		b = ((a * b) + c) % m;
 		return b;
 		}
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++) {
-		} if (playerNumber > receivedRandomNumber()) {
+		for (int i = 0; i < 100; i++)
+		if (playerNumber > receivedRandomNumber()) {
 			System.out.println("Введенное вами число больше того, что загадал компьютер");
 		} else if (playerNumber < receivedRandomNumber()) {
 			System.out.println("Введенное вами число меньше того, что загадал компьютер");
