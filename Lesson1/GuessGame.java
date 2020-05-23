522e3fb6d3a04e8cd0b929aaf717baa496ca2ad5
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-05-23 17:45:47 +0300

    Исправленны неточности в коде игры Угадайка

--- Lesson1/GuessGame.java
+++ Lesson1/GuessGame.java
@@ -1,16 +1,16 @@
 public class GuessGame {
-	public static int playerNumber = 1;
-	public static int a = 64;
+	public static int playerNumber = 98;
+	public static int a = 1;
 	public static int c = 99;
-	public static int m = 83;
-	public static int b = 2;
+	public static int m = 100;
+	public static int b = 17;
 	public static int receivedRandomNumber() {
 		b = ((a * b) + c) % m;
 		return b;
 		}
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++)
-		if (playerNumber > receivedRandomNumber()) {
+		for (int i = 0; i < 100; i++) 
+			if (playerNumber > receivedRandomNumber()) {
 			System.out.println("Введенное вами число больше того, что загадал компьютер");
 		} else if (playerNumber < receivedRandomNumber()) {
 			System.out.println("Введенное вами число меньше того, что загадал компьютер");
