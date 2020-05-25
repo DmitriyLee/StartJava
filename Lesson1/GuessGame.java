1ba20e9e1e511ae9cf39c2481b859ab3ca524ad4
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-05-25 17:46:42 +0300

    Исправленны неточности в коде игры Угадайка

--- Lesson1/GuessGame.java
+++ Lesson1/GuessGame.java
@@ -1,22 +1,28 @@
 public class GuessGame {
-	public static int playerNumber = 98;
-	public static int a = 1;
-	public static int c = 99;
-	public static int m = 100;
-	public static int b = 17;
-	public static int receivedRandomNumber() {
-		b = ((a * b) + c) % m;
-		return b;
-		}
+	private static int compNum = 17;
+	private static int a = 1;
+	private static int c = 99;
+	private static int m = 100;
+	private static int playerNum = 13;
+
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++) 
-			if (playerNumber > receivedRandomNumber()) {
-			System.out.println("Введенное вами число больше того, что загадал компьютер");
-		} else if (playerNumber < receivedRandomNumber()) {
-			System.out.println("Введенное вами число меньше того, что загадал компьютер");
+		for (int i = 0; i <= 100; i++) {
+		if (compNum > numberRandomPlayer()) {
+			System.out.print(compNum + " " + ">" + " " + numberRandomPlayer());
+			System.out.println(" = " + "Введенное вами число меньше того, что загадал компьютер");
+		} else if (compNum < numberRandomPlayer()) {
+			System.out.print(compNum + " " + "<" + " " + numberRandomPlayer());
+			System.out.println(" = " + "Введенное вами число больше того, что загадал компьютер");
 		} else {
-			System.out.println("Вы угадали!");
+			System.out.print(compNum);
+			System.out.println(" = " + "Вы угадали!");
 			break;
+			}
 		}
+	}
+
+	private static int numberRandomPlayer() {
+		playerNum = ((a * playerNum) + c) % m;
+		return playerNum;
 	}
 }
