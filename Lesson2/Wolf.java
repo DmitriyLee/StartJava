e1192e49c738320f19300c4ef80fab2bda09dfc7
Dmitriy Tereshchenko <ya.liss999@yandex.ru>
2020-06-08 22:10:56 +0300

    Внесены изменения в файлы Wolf и WolfTest, согласно ДЗ #3 к Уроку #2

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,4 +1,4 @@
-// Домашнее задание:
+// Домашнее задание II:
 
 // Создайте класс Wolf
 // напишите в нем поля: пол, кличка, вес, возраст, окрас
@@ -12,33 +12,96 @@
 // вызовите методы объекта
 // Покажите Д/З наставнику
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+// Домашнее задание III:
+
+// Модифицируйте класс Wolf
+// у всех полей класса напишите модификатор доступа private
+// у всех методов класса напишите модификатор доступа public
+// для доступа к полям создайте геттеры и сеттеры
+// в сеттере поля age реализуйте проверку: если возраст волка > 8 лет, то выведите сообщение "Некорректный возраст"
+// Модифицируйте класс WolfTest
+// с помощью сеттеров присвойте полям экземпляра класса Wolf какие-то значения
+// считайте эти значения из полей с помощью геттеров и отобразите в консоли
+// Покажите Д/З наставнику
+
 public class Wolf {
 
-	String gender = "cable";
-	String nickName = "Vir";
-	float weight = 45f;
-	int age = 5;
-	String color = "gray";
+	private String gender = "male";
+	private String nickName = "Vir";
+	private float weight = 45f;
+	private int age = 7;
+	private String color = "gray";
 
-	void go() {
-		System.out.println("Vir начал движени!");
+// gender
+	public String getGender() {
+		return gender;
 	}
 
-	void sit() {
-		System.out.println("Vir сел!");
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 
-	void run() {
-		System.out.println("Vir перешел на бег");
+// nickName
+	public String getNickName() {
+		return nickName;
 	}
 
-	boolean howls() {
-		System.out.println("Vir начал выть. Завёт стаю!");
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+// weight
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+// weight
+	public int getAga() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age > 8) {
+			System.out.println("Некорректный возраст");
+		} else {
+			this.age = age;
+		}
+	}
+
+// color
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+// methods
+	public void go() {
+		System.out.println("Волк начал движени!");
+	}
+
+	public void sit() {
+		System.out.println("Волк сел!");
+	}
+
+	public void run() {
+		System.out.println("Волк перешел на бег");
+	}
+
+	public boolean howls() {
+		System.out.println("Волк начал выть. Завёт стаю!");
 		return true;
 	}
 
-	String hunt() {
-		System.out.println("Vir охотиться вместе со стаей");
+	public String hunt() {
+		System.out.println("Волк охотиться вместе со стаей");
 		return "Стая поймала добычу!";
 	}
 
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,12 +1,34 @@
 public class WolfTest {
 
 	public static void main(String[] args) {
+
 		Wolf wolfOne = new Wolf();
-		System.out.println("gender = " + wolfOne.gender);
-		System.out.println("nickName = " + wolfOne.nickName);
-		System.out.println("weight = " + wolfOne.weight);
-		System.out.println("age = " + wolfOne.age);
-		System.out.println("color = " + wolfOne.color);
+
+//gender
+		System.out.println("gender = " + wolfOne.getGender());
+		wolfOne.setGender("female");
+		System.out.println("new gender = " + wolfOne.getGender());
+
+//nickName
+		System.out.println("nickName = " + wolfOne.getNickName());
+		wolfOne.setNickName("Rodis");
+		System.out.println("new nickName = " + wolfOne.getNickName());
+
+//weight
+		System.out.println("weight = " + wolfOne.getWeight());
+		wolfOne.setWeight(50);
+		System.out.println("new weight = " + wolfOne.getWeight());
+
+//age
+		System.out.println("age = " + wolfOne.getAga());
+		wolfOne.setAge(5);
+		System.out.println("new age = " + wolfOne.getAga());
+
+//color
+		System.out.println("color = " + wolfOne.getColor());
+		wolfOne.setColor("white");
+		System.out.println("new color = " + wolfOne.getColor());
+
 		System.out.println(wolfOne.howls());
 		System.out.println(wolfOne.hunt());
 	}
